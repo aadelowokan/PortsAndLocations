b4fede3a641d7d68016f060bab64c6e8271aa588
aadelowokan <adedayo.adelowokan@gmail.com>
2015-10-09 23:58:36 +0100

    ale to print the minimum distance for each location and the amount of material produced at the location

--- Assignment01_15204151.py
+++ Assignment01_15204151.py
@@ -37,12 +37,16 @@ with con:
     locations = cur.fetchall()    
     i = 0
     j = 0
-    dista = np.zeros((3, 10))
-    for port in ports:
+    dista = np.zeros((10, 3))
+    for location in locations:
         j = 0
-        for location in locations:
-            print "hu"
-            dista[i][j] = distance(port[0], port[1], location[0], location[1])
-            print "ja"
+        for port in ports:
+            dista[i][j] = distance(location[0], location[1], port[0], port[1])
             j += 1
         i += 1
+    
+    i = 0
+    for location in locations:
+        print(dista[i,:].min(), " ", location[2])
+        i += 1
+    
