34151abba201056c89de5213704106b4f7e1c14d
aadelowokan <adedayo.adelowokan@gmail.com>
2015-10-10 02:30:02 +0100

    adding comments to code and documentation also

--- Assignment01_15204151.py
+++ Assignment01_15204151.py
@@ -11,6 +11,7 @@ import numpy as np
 
 con = lite.connect('renewable.db')
 
+# function to calculate the distance between 2 points
 def distance(x1, y1, x2, y2):
     x = (x2 - x1)**2
     y = (y2 - y1)**2
@@ -24,12 +25,17 @@ with con:
     
     cur.execute("SELECT * FROM ports;")    
     ports = cur.fetchall()
+    
     cur.execute("SELECT * FROM location;")
     locations = cur.fetchall()
     
     i = 0
     j = 0
+    # 2d numpy array
     dista = np.zeros((10, 4))
+    
+    # for loop with in a for loop
+    # to calculate the distance from each location to all 3 ports
     for location in locations:
         j = 0
         for port in ports:
@@ -39,19 +45,21 @@ with con:
         i += 1
     
     i = 0
-    for location in locations:
-        print dista[i,:].min(), " ", dista[i, 3]
-        i += 1
-    
-    i = 0
+    # numpy array
     array = np.zeros(10)
+    
+    # for loop to calculate the rate which the material will be transported
+    # in relation to the min distance needed to travel to get to a port
     for row in dista:
         array[i] = dista[i, 3]/dista[i,:].min()
         i+=1
         
+    # print the location number
     print "Location number ", array.argmax()
 
     i = 0
+    # for loop to compare the min distance for that location to each one
+    # in order to get the port number
     for x in range(3):
         if(dista[array.argmax(), i] == dista[array.argmax(),:].min()):
             print "Port number ", i
